9f82363ac26d1022ce4a48736b7b283950c78891
BiscuitCuit15 <ldescombes@edenschool.fr>
2024-11-15 15:10:38 +0100

    feat (ex06) : correct ex06

--- ex06.js
+++ ex06.js
@@ -1,16 +1,16 @@
 // ex06.js
 
-function getReduction(price, age) {
-    if(age < 25){
-        return price * 0;
-    } else if(age > 25){
-        return price * 0.3;
-    } else if(age < 18){
-        return price * 0.5;
-    } else if(age < 5){
-        return price * 1;
-    }
-}
+// function getReduction(price, age) {
+//     if(age < 25){
+//         return price * 0;
+//     } else if(age <= 25){
+//         return price * 0.3;
+//     } else if(age <= 18){
+//         return price * 0.5;
+//     } else if(age < 5){
+//         return price * 1;
+//     }
+// }
 
-console.log(getReduction(10, 5)); // Résultat attendu: 0
-console.log(getReduction(150, 25)); // Résultat attendu: 45
+// console.log(getReduction(10, 5)); // Résultat attendu: 0
+// console.log(getReduction(150, 25)); // Résultat attendu: 45
