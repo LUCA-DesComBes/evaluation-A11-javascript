313484e89a037119b80efe60290c3785441eb80d
BiscuitCuit15 <ldescombes@edenschool.fr>
2024-11-15 14:59:42 +0100

    feat (ex07) : correct ex07

--- ex07.js
+++ ex07.js
@@ -3,11 +3,11 @@
 function isNationalIDValid(name, age, zip, password) {
 	if(name != ""){
         return true;
-    } else if(age < 18 || age > 65){
+    } else if(age > 18 || age < 65){
         return true;
-    } else if(zip > 99999 && zip < 0){
+    } else if(zip < 99999 && zip > 0){
         return true;
-    } else if(password == "qwerty" || password == "azerty" || password == "1234"){
+    } else if(password != "qwerty" || password != "azerty" || password != "1234"){
         return true;
     } else {
         return false;
