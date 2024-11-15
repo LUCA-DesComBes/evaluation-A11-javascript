985c4e4fa8e19d981db47bc8bea477790f220399
BiscuitCuit15 <ldescombes@edenschool.fr>
2024-11-15 15:32:42 +0100

    feat (ex06) : add ex06

--- ex06.js
+++ ex06.js
@@ -2,15 +2,15 @@
 
 function getReduction(price, age) {
     if(age < 25){
-        return price * 0;
+        return price * 1;
     } else if(age <= 25){
-        return price * 0.3;
+        return price * 0.7;
     } else if(age <= 18){
         return price * 0.5;
     } else if(age < 5){
-        return price * 1;
+        return price * 0;
     }
 }
 
-console.log(getReduction(10, 5)); // Résultat attendu: 0
-console.log(getReduction(150, 25)); // Résultat attendu: 45
+console.log(getReduction(10, 5)); // Résultat attendu: 5
+console.log(getReduction(150, 25)); // Résultat attendu: 105
